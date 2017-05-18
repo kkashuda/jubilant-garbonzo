13886d9663ae7ec00fc892dc2a0e618a174a225c
kkashuda <kkashuda@gmail.com>
2017-05-17 19:10:15 -0500

    add acts as votable migration and add it to pins model

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :pins
   root to: "pins#index"
+
+  resources :pins do
+    put 'like', to: 'pins#upvote'
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

e28900261c4e6be462e601fcd1e03d57cc7b638e
kkashuda <kkashuda@gmail.com>
2017-05-21 13:31:14 -0500

    validate link

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -6,6 +6,7 @@ class Pin < ApplicationRecord
   validates :title, length: { minimum: 5 }
   validates :title, presence: true 
   validates :description, presence: true 
+  validates :link, :format => URI::regexp(%w(http https))
 
   has_attached_file :image, :styles => { :medium => "300x300>" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/

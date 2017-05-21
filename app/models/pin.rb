90b20ff2f0969bfd9cf410101f1b3e716f5238bf
kkashuda <kkashuda@gmail.com>
2017-05-21 13:49:11 -0500

    allow no link and center error messages

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -6,8 +6,9 @@ class Pin < ApplicationRecord
   validates :title, length: { minimum: 5 }
   validates :title, presence: true 
   validates :description, presence: true 
-  validates :link, :format => URI::regexp(%w(http https))
+  validates :link, :format => URI::regexp(%w(http https)), :allow_blank => true
 
   has_attached_file :image, :styles => { :medium => "300x300>" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+    
 end

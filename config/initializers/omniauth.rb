b765be36300f7481f3ddae563b902c37c34b4f31
kkashuda <kkashuda@gmail.com>
2017-05-22 18:29:55 -0500

    update OAuth to work without having to set id and secret in terminal

--- config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '1824722077856806', 'bfc55638dbe5bfad960d0c728690f2ab'
-end

93dc8f6279c40048afcaca9b0b19356ec391436b
kkashuda <kkashuda@gmail.com>
2017-06-26 20:13:53 -0500

    add strong params to comment controller and new route

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,15 @@
 class CommentsController < ApplicationController
+  def new
+  	@user = current_user 
+  	@comment = Comment.new  
+  end 
 
+  
+
+
+  private 
+
+  def comment_params
+  	params.require(:comment).permit(:content, :title, :id, :username)
+  end 
 end

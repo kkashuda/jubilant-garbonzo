5ea7e4502a9cd0ab3fc0d541fa2a2859886ed6fc
kkashuda <kkashuda@gmail.com>
2017-06-27 18:35:24 -0500

    add create action for creating a comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,6 +12,19 @@ class CommentsController < ApplicationController
   	end  
   end 
 
+  def create
+  	@comment = Comment.new 
+  	respond_to do |format|
+  	if @comment.save 
+  		format.html {redirect_to @comment, notice: 'Comment was successfully created.'}
+  		format.json {render action :show, status: :created, location: @comment}
+  	else 
+  		format.html {render action: 'new'}
+  		format.json {render json: @comment.errors, status :unprocessable_entity}
+  	end 
+  end
+end  
+
 
 
   private 

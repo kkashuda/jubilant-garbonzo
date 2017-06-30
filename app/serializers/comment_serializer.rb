959f95aada559b14ec3348dea755f04392f219bf
kkashuda <kkashuda@gmail.com>
2017-06-30 17:49:05 -0500

    get request for loading more comments

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,4 +1,4 @@
 class CommentSerializer < ActiveModel::Serializer
   attributes :id, :title, :content, :username, :pin_id
   belongs_to :pin
-end
+end 
--- app/serializers/pin_serializer.rb
+++ app/serializers/pin_serializer.rb
@@ -1,4 +1,5 @@
 class PinSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :link 
-  has_many :comments
+  has_many :comments 
+  belongs_to :user
 end

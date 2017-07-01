888591a5d318848548b2d931621d1c9da3f9906c
kkashuda <kkashuda@gmail.com>
2017-07-01 17:45:52 -0500

    add load comments to next pin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  serialization_scope :view_context
+
 
   def after_sign_in_path_for(resource)
     request.env['omniauth.origin'] || root_path
--- app/serializers/pin_serializer.rb
+++ app/serializers/pin_serializer.rb
@@ -1,5 +1,5 @@
 class PinSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :link 
+  attributes :id, :title, :description, :link, :image
   has_many :comments 
   belongs_to :user
 end

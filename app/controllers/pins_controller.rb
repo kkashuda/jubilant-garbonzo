69c0e97b45b7862db2c25dbe966c58c6466d8fb7
kkashuda <kkashuda@gmail.com>
2017-07-01 19:12:01 -0500

    remove pry

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,4 +1,3 @@
-require 'pry'
 class PinsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_pin, only: [:show, :edit, :update, :destroy, :upvote]
